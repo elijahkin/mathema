2915eecfaf894dff56b47d1164fcafd519ed2fb2
Eli Kin <3limkin@gmail.com>
2020-09-12 16:08:36 -0400

    Implemented scalarMultiple and unitVector methods

--- Vector.java
+++ Vector.java
@@ -1,31 +1,49 @@
+package mathema;
 
 public class Vector {
 	
-	// fields
+	// FIELDS
+	
 	private double x;
 	private double y;
 	private double z;
 	
-	// constructors
+	// CONSTRUCTORS
+	
 	public Vector(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 	
-	// methods
+	// METHODS
+	
+	// allows vectors to be printed as a string in angle bracket notation
 	public String toString() {
 		return String.format("<%f, %f, %f>", x, y, z);
 	}
 	
+	// returns the magnitude of a vector
 	public double magnitude() {
 		return Math.sqrt(Math.pow(this.x, 2)+Math.pow(this.y, 2)+Math.pow(this.z, 2));
 	}
 	
+	// returns the unit vector of a vector 
+	public Vector unitVector() {
+		return this.scalarMultiple(1 / this.magnitude());
+	}
+	
+	// returns the sum of two vectors
 	public Vector add(Vector v) {
 		return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
 	}
 	
+	// returns parallel vector multiplied by s
+	public Vector scalarMultiple(double s) {
+		return new Vector(s * this.x, s * this.y, s * this.z);
+	}
+	
+	// returns the dot product of two vectors
 	public double dot(Vector v) {
 		return (this.x * v.x)+(this.y * v.y)+(this.z * v.z);
 	}
@@ -36,5 +54,6 @@ public class Vector {
 		System.out.println(v.add(w));
 		System.out.println(v.dot(w));
 		System.out.println(v.magnitude());
+		System.out.println(v.unitVector());
 	}
 }
